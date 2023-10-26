d15ddad6e1387fd3805d75c84b7e28a4ba89787a
oussama Tahiri <you@example.com>
2023-10-26 17:51:04 +0200

    ds

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 
-const rulesButton = document.getElementById("rulesButton");
-const rules = document.querySelector(".rules");
-const closeButton = document.querySelector(".close_btn");
-// Function to show the rules
-function showRules() {
-  rulesContainer.style.display = "block";
-}
-// Function to hide the rules
-function hideRules() {
-  rulesContainer.style.display = "none";
-}
-ggggggggg
-// Attach event listener to the "RULES" button
-rulesButton.addEventListener("click", showRules);
+document.addEventListener("DOMContentLoaded", function() {
+  const rulesButton = document.getElementById("rulesButton");
+  const rules = document.querySelector(".rules");
+  const rulesContainer = document.querySelector(".rules_container");
+  const closeButton = document.querySelector(".close_btn");
 
-// Attach event listener to the "close" button
-closeButton.addEventListener("click", hideRules);
+  // Function to show the rules
+  function showRules() {
+    rules.style.display = "block";
+  }
+
+  // Function to hide the rules
+  function hideRules() {
+    rules.style.display = "none";
+  }
+  // Attach event listener to the "RULES" button
+  rulesButton.addEventListener("click", showRules, hideRules);
+
+  // Attach event listener to the "close" button
+  closeButton.addEventListener("click", hideRules);
+});
+
